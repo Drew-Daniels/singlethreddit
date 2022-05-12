66d6ca69a3ffbb1a0b3666a67df387dd9221c0a3
Drew Daniels <daniels_drew@protonmail.com>
2022-05-12 15:53:35 -0500

    Added more mui dependencies and have added a navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Reset } from 'styled-reset';
 import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
       <div className="App">
         <Reset />
+        <Navbar />
         <Outlet />
       </div>
   );
