28d6127d3794284757cf15a8e04ccf3c3a1f0bab
Drew Daniels <daniels_drew@protonmail.com>
2022-05-12 15:34:19 -0500

    Added create-react-app-configuration-override package to use styled-components with create-react-app and deleted Home.scss file.

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,8 +1,6 @@
 import { IconContext } from 'react-icons';
 import {RiAliensFill as AppIcon } from 'react-icons/ri';
 
-import './Home.scss';
-
 export default function Home(props) {
 
     return (
