3b303b18ad3a33707c095a1ad9b23c8c4d04a45b
Drew Daniels <daniels_drew@protonmail.com>
2022-05-19 14:08:12 -0500

    Committing before making changes to create a reusable layout component.

---
+++ src/components/Rules/Rules.jsx
@@ -0,0 +1,8 @@
+import Rule from './Rule';
+
+export default function Rules(props) {
+
+    return (
+        <div>Rules here</div>
+    )
+}
--- src/routes/SubmitPostPage.jsx
+++ src/routes/SubmitPostPage.jsx
@@ -1,6 +1,26 @@
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Sidebar from '../components/Sidebar';
+import Rules from '../components/Rules/Rules';
+
 export default function SubmitPostPage(props) {
 
     return (
-        <div>SubmitPostPage here</div>
+        // <Container maxWidth={false}>
+            
+        //     <Sidebar>
+        //         <Rules />
+        //     </Sidebar>
+        // </Container>
+        <Grid container spacing={2} sx={{ marginTop: '1rem'}}>
+            <Grid item md={2}/>
+            <Grid item xs={12} sm={9} md={5}>
+                
+            </Grid>
+            <Grid item sm={0} md={3}>
+                <Sidebar />
+            </Grid>
+            <Grid item md={2} />
+        </Grid>
     )
 }
