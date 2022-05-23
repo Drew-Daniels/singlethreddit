003c3cb42f24c24f26af0c1acd4147c18f3059c0
Drew Daniels <daniels_drew@protonmail.com>
2022-05-23 09:22:06 -0500

    Updated post button to be a link to the submit page.

--- src/components/Buttons/PostButton.jsx
+++ src/components/Buttons/PostButton.jsx
@@ -1,24 +1,22 @@
 import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import { Link as RouterLink } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 import Input from '@mui/material/Input';
-import { useNavigate } from 'react-router-dom';
 
 export default function PostButton(props) {
     const { userAvatar } = props;
-    const navigate = useNavigate();
-
-    function handleClick() {
-        navigate('submit')
-    }
 
     return (
-        <Grid component='a' container sx={{ border: '1px solid yellow', marginBottom: '1rem'}} onClick={handleClick}>
-            <Grid item xs={2} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Avatar src={userAvatar} alt='User Avatar'></Avatar>
+        <Link component={RouterLink} to='submit'>
+            <Grid component='a' container sx={{ border: '1px solid yellow', marginBottom: '1rem'}}>
+                <Grid item xs={2} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <Avatar src={userAvatar} alt='User Avatar'></Avatar>
+                </Grid>
+                <Grid item xs={10} sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Input placeholder='Create Post' />
+                </Grid>
             </Grid>
-            <Grid item xs={10} sx={{ display: 'flex', alignItems: 'center' }}>
-                <Input placeholder='Create Post' />
-            </Grid>
-        </Grid>
+        </Link>
     )
 }
