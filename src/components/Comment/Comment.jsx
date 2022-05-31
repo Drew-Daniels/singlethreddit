d67d0fdd156f8089ee001ee6916608582469ada4
Drew Daniels <daniels_drew@protonmail.com>
2022-05-31 16:48:47 -0500

    Now can make nested (comment below comment below post) comment

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -8,14 +8,14 @@ import CommentFooter from './CommentFooter';
 
 export default function Comment(props) {
 
-    const { body, timeCreated, userName, userAvatarURL, selectedGroup, parentId, children } = props;
+    const { body, timeCreated, userName, userAvatarURL, selectedGroup, id, parentId, children } = props;
     return (
         <Container maxWidth='false'>
             <Card>
                 <Avatar src={userAvatarURL} />
                 <CommentHeader userName={userName} />
                 <CommentMain body={body} />
-                <CommentFooter selectedGroup={selectedGroup} parentId={parentId} />
+                <CommentFooter selectedGroup={selectedGroup} parentId={id} />
                 {children &&
                     <Comments comments={children} selectedGroup={selectedGroup} />
                 }
