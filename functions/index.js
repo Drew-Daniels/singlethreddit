cb776eb3682f1c32bc866dedb18fab7fb0fe1666
Drew Daniels <daniels_drew@protonmail.com>
2022-06-01 17:11:00 -0500

    Created a Google Cloud function to dynamically calculate 'karma' as a field that can be used to store the number of upvotes against downvotes for each comment.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
-// const functions = require("firebase-functions");
+// 'firebase-functions' used to create Cloud Functions and set up triggers
+const functions = require("firebase-functions");
+// 'firebase-admin' required to access firestore
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +11,16 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.getKarma = functions.firestore.document('/comments/{documentId}')
+    // TODO: See if this function can just be run on after changes to upvoters/downvoters?
+    .onWrite((change, context) => {
+        const ref = change.after.ref;
+        const data = change.after.data();
+        if (data) {
+            // data will be undefined if the the write update was a delete
+            const {upvoters, downvoters} = change.after.data();
+            const karma = upvoters.length - downvoters.length;
+            return ref.set({ karma }, { merge: true });
+        }
+    });
