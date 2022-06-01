26cd41a60096659777b990698ef8f03f2dfcf1ab
Drew Daniels <daniels_drew@protonmail.com>
2022-06-01 17:16:17 -0500

    Removed commented out code from Google Cloud function example.

--- functions/index.js
+++ functions/index.js
@@ -4,14 +4,6 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 exports.getKarma = functions.firestore.document('/comments/{documentId}')
     // TODO: See if this function can just be run on after changes to upvoters/downvoters?
     .onWrite((change, context) => {
