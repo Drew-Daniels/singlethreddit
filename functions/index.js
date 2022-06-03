e8ef940c7bccbcd59b25f21e87cac3d9c55eda77
Drew Daniels <daniels_drew@protonmail.com>
2022-06-02 20:48:00 -0500

    Have begun working on a cloud function that will be triggered when a comment is created and adds a mediaURL field to each comment if there was media uploaded with the post.

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,31 @@
-// "firebase-functions" used to create Cloud Functions and set up triggers
+// const { POST_MEDIA_STORAGE_FOLDER_NAME } = require("../src/constants");
 const functions = require("firebase-functions");
-// "firebase-admin" required to access firestore
 const admin = require("firebase-admin");
-admin.initializeApp();
+const { ref, getDownloadURL } = require("firebase/storage");
 
-exports.getKarma = functions.firestore.document("/comments/{documentId}")
+const app = admin.initializeApp();
+const storage = admin.storage(app);
+
+exports.setKarma = functions.firestore.document("/comments/{documentId}")
     .onWrite((change, context) => {
         const ref = change.after.ref;
         const data = change.after.data();
-            if (data) {
-                const {upvoters, downvoters} = change.after.data();
-                const karma = upvoters.length - downvoters.length;
-                return ref.set({karma}, {merge: true});
-            }
+        if (data) {
+            const {upvoters, downvoters} = change.after.data();
+            const karma = upvoters.length - downvoters.length;
+            return ref.set({karma}, {merge: true});
         }
-    );
+    }
+);
+
+exports.setMediaURL = functions.firestore.document("/comments/{documentId}")
+    .onCreate(async (snapshot, context) => {
+        const storageRef = ref(storage, 'post-media/' + snapshot.id);
+        console.log(storageRef);
+        const mediaURL = await getDownloadURL(storageRef);
+        if (mediaURL) {
+            return snapshot.ref.set({mediaURL}, {merge: true});
+        }
+    });
+
+// exports.setAvatarURL = functions.firestore.document("/comments/{documentId}")
