5e683f9683c494b03e6c96985ba9b21c72d6fe9a
Drew Daniels <daniels_drew@protonmail.com>
2022-07-10 17:34:07 -0500

    No longer including "export " on each line. Also added a todo to add groups that can be importing during development.

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,36 @@
-export const COMMENTS_COLLECTION_NAME = 'comments';
-export const GROUPS_COLLECTION_NAME = 'groups';
-export const GROUP_AVATARS_STORAGE_FOLDER_NAME = 'group-avatars';
-export const GROUP_BANNERS_STORAGE_FOLDER_NAME = 'group-banners';
-export const POST_MEDIA_STORAGE_FOLDER_NAME = 'post-media';
+const COMMENTS_COLLECTION_NAME = 'comments';
+const GROUPS_COLLECTION_NAME = 'groups';
+const GROUP_AVATARS_STORAGE_FOLDER_NAME = 'group-avatars';
+const GROUP_BANNERS_STORAGE_FOLDER_NAME = 'group-banners';
+const POST_MEDIA_STORAGE_FOLDER_NAME = 'post-media';
 
 const KILOBYTE = 1000;
-export const MAX_POST_MEDIA_SIZE_BYTES = KILOBYTE * 15;
-export const MAX_GROUP_AVATAR_IMAGE_SIZE_BYTES = KILOBYTE * 15;
-export const MAX_GROUP_BANNER_IMAGE_SIZE_BYTES = KILOBYTE * 250;
+const MAX_POST_MEDIA_SIZE_BYTES = KILOBYTE * 15;
+const MAX_GROUP_AVATAR_IMAGE_SIZE_BYTES = KILOBYTE * 15;
+const MAX_GROUP_BANNER_IMAGE_SIZE_BYTES = KILOBYTE * 250;
+
+
+// TODO: Add group data for importing during development
+// development data for generating groups
+const GROUP_1 = {
+
+}
+const GROUP_2 = {
+
+}
+const GROUP_3 = {
+
+}
+
+
+export {
+    COMMENTS_COLLECTION_NAME,
+    GROUPS_COLLECTION_NAME,
+    GROUP_AVATARS_STORAGE_FOLDER_NAME,
+    GROUP_BANNERS_STORAGE_FOLDER_NAME,
+    POST_MEDIA_STORAGE_FOLDER_NAME,
+    MAX_POST_MEDIA_SIZE_BYTES,
+    MAX_GROUP_AVATAR_IMAGE_SIZE_BYTES,
+    MAX_GROUP_BANNER_IMAGE_SIZE_BYTES,
+    GROUP_1,
+}
